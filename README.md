f89539eb051ed5c3d67ceb89b4934e0ccffc9b33
BigETI <bigeti.ethem@gmail.com>
2019-08-28 13:39:20 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Save game system for Unity
+
+## Description
+This package contains a save game system for Unity.
+
+## License
+[The license](https://github.com/BigETI/UnitySaveGame/blob/master/LICENSE) for this package is available at https://github.com/BigETI/UnitySaveGame/blob/master/LICENSE
---
+++ Scripts/Abstract/ASaveGameData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnitySaveGame.Data;
+
+/// <summary>
+/// Unity save game namespace
+/// </summary>
+namespace UnitySaveGame
+{
+    /// <summary>
+    /// Save game data interface
+    /// </summary>
+    [Serializable]
+    public abstract class ASaveGameData
+    {
+        /// <summary>
+        /// Last save date and time
+        /// </summary>
+        [SerializeField]
+        private DateTimeData lastSaveDateTime;
+
+        /// <summary>
+        /// Last save date and time
+        /// </summary>
+        public DateTime LastSaveDateTime
+        {
+            get
+            {
+                if (lastSaveDateTime == null)
+                {
+                    lastSaveDateTime = new DateTimeData(DateTime.Now);
+                }
+                return lastSaveDateTime.DateTime;
+            }
+            set => lastSaveDateTime = new DateTimeData(value);
+        }
+
+        /// <summary>
+        /// Update last save date and time
+        /// </summary>
+        public void UpdateLastSaveDateTime()
+        {
+            lastSaveDateTime = new DateTimeData(DateTime.Now);
+        }
+    }
+}
